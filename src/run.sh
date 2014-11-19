3b3bcf9db4d6d715fa3a9b3b8c20933f02b6d6e8
Gaurav Ahuja <ga2371@columbia.edu>
2014-11-19 02:45:32 -0500

    Modifying run.sh, Adding gitignore, group 4 and 5 nov 9 submission

--- src/run.sh
+++ src/run.sh
@@ -7,12 +7,12 @@ L=${3:-4}
 W=${4:-1}
 GUI=${5:-true}
 GROUP0=${6:-group6}
-GROUP1=${7:-group6}
-GROUP2=${8:-group6}
-GROUP3=${9:-group6}
+GROUP1=${7:-dumb}
+GROUP2=${8:-group4}
+GROUP3=${9:-group5}
 T=${10:-100}
 
-javac outpost/sim/outpost.java
+javac outpost/sim/Outpost.java
 javac outpost/$GROUP0/Player.java
 javac outpost/$GROUP1/Player.java
 javac outpost/$GROUP2/Player.java
